5b3bcb16eda130450d066e9690131961481a1860
rivaldofez <rivaldofez@gmail.com>
2023-01-28 19:12:21 +0700

    set mapview and add constraint

--- Proximate/ViewController.swift
+++ Proximate/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    lazy var mapView: MKMapView = {
+       let map = MKMapView()
+        map.showsUserLocation = true
+        map.translatesAutoresizingMaskIntoConstraints = false
+        return map
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupUI()
     }
-
+    
+    private func setupUI(){
+        view.addSubview(mapView)
+        
+        //add constraint mapview
+        NSLayoutConstraint.activate([
+            mapView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            mapView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            
+        
+        ])
+        
+    }
 
 }
 
