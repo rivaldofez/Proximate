01fb7af6ffc5492bb9723233f7d2ab8752e549fc
rivaldofez <rivaldofez@gmail.com>
2023-01-28 23:08:57 +0700

    add button directional and phone

--- Proximate/Controllers/PlaceDetailViewController.swift
+++ Proximate/Controllers/PlaceDetailViewController.swift
@@ -10,7 +10,41 @@ import UIKit
 class PlaceDetailViewController: UIViewController {
     let place: PlaceAnnotation
     
+    lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.font = .preferredFont(forTextStyle: .title1)
+        return label
+    }()
     
+    lazy var addressLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.font = .preferredFont(forTextStyle: .body)
+        label.alpha = 0.4
+        return label
+    }()
+    
+    lazy var directionButton: UIButton = {
+        var config = UIButton.Configuration.bordered()
+        let button = UIButton(configuration: config)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Directions", for: .normal)
+        return button
+    }()
+    
+    lazy var callButton: UIButton = {
+        var config = UIButton.Configuration.bordered()
+        let button = UIButton(configuration: config)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Call", for: .normal)
+        return button
+    }()
     
     
     init(place: PlaceAnnotation){
@@ -21,7 +55,7 @@ class PlaceDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .black
+        view.backgroundColor = .white
     }
     
     required init?(coder: NSCoder) {
@@ -29,6 +63,37 @@ class PlaceDetailViewController: UIViewController {
     }
     
     private func setupUI(){
+        let stackView = UIStackView()
+        stackView.alignment = .leading
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = UIStackView.spacingUseSystem
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
         
+        nameLabel.text = place.name
+        addressLabel.text = place.address
+        
+        stackView.addArrangedSubview(nameLabel)
+        stackView.addArrangedSubview(addressLabel)
+        
+        let contactStackView = UIStackView()
+        contactStackView.translatesAutoresizingMaskIntoConstraints = false
+        contactStackView.axis = .horizontal
+        contactStackView.spacing = UIStackView.spacingUseSystem
+        
+        contactStackView.addArrangedSubview(directionButton)
+        contactStackView.addArrangedSubview(callButton)
+        stackView.addArrangedSubview(contactStackView)
+        
+        
+        NSLayoutConstraint.activate([
+            nameLabel.widthAnchor.constraint(equalToConstant: view.bounds.width - 20)
+        ])
+        
+        view.addSubview(stackView)
+        
+
+    
     }
 }
